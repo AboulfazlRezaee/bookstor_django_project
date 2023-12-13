d496fdc82258dc5d82699522c69c39a1e8af30c0
AboulfazlRezaee <rezaee.aboulfazl@gmail.com>
2023-12-13 20:56:38 +0330

    add book list view

--- books/models.py
+++ books/models.py
@@ -7,3 +7,6 @@ class Book(models.Model):
     author = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.author} : {self.title}"
--- books/views.py
+++ books/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.views import generic
 
+from .models import Book
 # Create your views here.
+
+
+class BookListView(generic.ListView):
+    model = Book
+    template_name = 'books/book_list.html'
+    # paginate_by = 10
+    context_object_name = 'books'
+    # def book_detail(request, pk):
+    #     book = get_object_or_404(Book, pk=pk)
+    #     return render(request, 'books/book_detail.html', {'book': book})
