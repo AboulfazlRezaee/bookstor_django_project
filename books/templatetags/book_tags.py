e1c4cbc9d7e598d53e3e3a64de26a38a82933867
AboulfazlRezaee <rezaee.aboulfazl@gmail.com>
2023-12-17 10:03:24 +0330

    add templatetags filter and dir to books app

---
+++ books/templatetags/book_tags.py
@@ -0,0 +1,8 @@
+from django import template
+
+# register = template.Library()
+
+# @register.filter
+# def get_item(dictionary, key):
+#     """Gets an item out of a dictionary given the key"""
+#     return dictionary.get(key)
